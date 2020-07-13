f616f42d650d9888dec5895aed9695116b7a5c7f
Amar kumar <32639666+amark720@users.noreply.github.com>
2020-07-13 23:06:53 +0530

    Add files via upload

---
+++ Text_to_Speech_App.py
@@ -0,0 +1,15 @@
+'''
+step1. GoTo Command Prompt and install package pyttsx3==2.71 using command 'pip install pyttsx3==2.71'
+step2. install package pywin32 using command pip install pywin32
+
+After that run the below code
+'''
+
+import pyttsx3
+
+data = input("Enter text which you want to convert to text:\n")
+
+engine = pyttsx3.init()
+engine.say(data)
+engine.runAndWait()
+
