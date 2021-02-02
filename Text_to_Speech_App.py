3b552b233a13b81903943fcc403c5fa477f5894c
Amar kumar <amark720@gmail.com>
2021-02-02 12:02:18 +0530

    Update Text_to_Speech_App.py

--- Text_to_Speech_App.py
+++ Text_to_Speech_App.py
@@ -7,7 +7,7 @@ After that run the below code
 
 import pyttsx3
 
-data = input("Enter text which you want to convert to text:\n")
+data = input("Enter text which you want to convert to Speech:\n")
 
 engine = pyttsx3.init()
 engine.say(data)
