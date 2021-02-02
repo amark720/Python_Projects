910635d0b06603dbcfdd4b6194ab928b34b2a043
Amar kumar <amark720@gmail.com>
2021-02-02 12:11:04 +0530

    Update OCR_Image_to_Text.py

--- OCR Image To Text App/OCR_Image_to_Text.py
+++ OCR Image To Text App/OCR_Image_to_Text.py
@@ -2,7 +2,7 @@
 step1. GoTo Command Prompt and install pytesseract package using command 'pip install pytesseract'
 step2. Goto this link - https://github.com/ub-mannheim/tesseract/wiki and download
 'tesseract-ocr-w64-setup-v5.0.0-alpha.20200328.exe (64 bit) resp.' setup and Install it on your machine.
-
+step3. Change the Image path in the below code.
 After that run the below code
 '''
 
@@ -13,7 +13,7 @@ pytesseract.pytesseract.tesseract_cmd = r"C:/Users/gsc-30431/AppData/Local/Tesse
 # Provide the path of tesseract.exe file which was installed in step2
 
 def convert():
-    img = Image.open('C:/Users/gsc-30431/PycharmProjects/test1.py/Python_Projects/OCR_Img_to_Text/img5.jpg')
+    img = Image.open('C:/Users/gsc-30431/PycharmProjects/test1.py/Python_Projects/OCR_Img_to_Text/img5.jpg')  # Update Image Path Here!
     # Provite the path of Image which is to be converted into text
     text = pytesseract.image_to_string(img)
     print(text)
